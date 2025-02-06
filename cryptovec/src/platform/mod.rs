4e43c142f3295de7869aa290e28f17a8cb36b104
Michael Gleason <gleasonm@microsoft.com>
2025-02-06 22:59:52 +0000

    Revert "Fix improper RSA key conversion from `ssh_key` crate (#400)"
    This reverts commit 2a4f451c8dcb7196534a3033b8c9177f817e6100.

--- cryptovec/src/platform/mod.rs
+++ cryptovec/src/platform/mod.rs
@@ -19,16 +19,18 @@ pub use windows::{memset, mlock, munlock};
 
 #[cfg(test)]
 mod tests {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
     use super::*;
 
-    #[test]
+    #[wasm_bindgen_test]
     fn test_memset() {
         let mut buf = vec![0u8; 10];
         memset(buf.as_mut_ptr(), 0xff, buf.len());
         assert_eq!(buf, vec![0xff; 10]);
     }
 
-    #[test]
+    #[wasm_bindgen_test]
     fn test_memset_partial() {
         let mut buf = vec![0u8; 10];
         memset(buf.as_mut_ptr(), 0xff, 5);
