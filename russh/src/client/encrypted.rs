63c747e9f20bcc66444b25030a85bd1c887f39b4
Michael Gleason <gleasonm@microsoft.com>
2025-02-06 23:00:05 +0000

    Revert "fixed #358 - relax strict kex checks to match OpenSSH"
    This reverts commit 3f7271b68414943056fbbb59390175a506813aa6.

--- russh/src/client/encrypted.rs
+++ russh/src/client/encrypted.rs
@@ -26,7 +26,7 @@ use crate::negotiation::{Named, Select};
 use crate::parsing::{ChannelOpenConfirmation, ChannelType, OpenChannelMessage};
 use crate::session::{Encrypted, EncryptedState, GlobalRequestResponse, Kex, KexInit};
 use crate::{
-    auth, msg, negotiation, Channel, ChannelId, ChannelMsg,
+    auth, msg, negotiation, strict_kex_violation, Channel, ChannelId, ChannelMsg,
     ChannelOpenFailure, ChannelParams, CryptoVec, Sig,
 };
 
@@ -70,9 +70,11 @@ impl Session {
                     None
                 };
 
-                if let Some(mut kexinit) = kexinit {
-                    if let Some(ref mut algo) = kexinit.algo {
-                        algo.strict_kex = algo.strict_kex || self.common.strict_kex;
+                if let Some(kexinit) = kexinit {
+                    if let Some(ref algo) = kexinit.algo {
+                        if self.common.strict_kex && !algo.strict_kex {
+                            return Err(strict_kex_violation(msg::KEXINIT, 0).into());
+                        }
                     }
 
                     let dhdone = kexinit.client_parse(
