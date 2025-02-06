4fdded679f2a548470f8546f423dde009795177f
Michael Gleason <gleasonm@microsoft.com>
2025-02-06 23:01:29 +0000

    Revert "fix: process multiple host entries (#331)"
    This reverts commit d5b303c43ac98d2d3dfef6f59ae7f98033658bfa.

--- russh-config/src/lib.rs
+++ russh-config/src/lib.rs
@@ -113,20 +113,15 @@ pub enum AddKeysToAgent {
 }
 
 pub fn parse(file: &str, host: &str) -> Result<Config, Error> {
-    let mut config = Config::default(host);
-    let mut matches_current = false;
+    let mut config: Option<Config> = None;
     for line in file.lines() {
         let tokens = line.trim().splitn(2, ' ').collect::<Vec<&str>>();
         if tokens.len() == 2 {
             let (key, value) = (tokens.first().unwrap_or(&""), tokens.get(1).unwrap_or(&""));
             let lower = key.to_lowercase();
-            if lower.as_str() == "host" {
-                matches_current = value
-                    .split_whitespace()
-                    .any(|x| check_host_against_glob_pattern(host, x));
-            }
-            if matches_current {
+            if let Some(ref mut config) = config {
                 match lower.as_str() {
+                    "host" => break,
                     "user" => {
                         config.user.clear();
                         config.user.push_str(value.trim_start());
@@ -171,10 +166,22 @@ pub fn parse(file: &str, host: &str) -> Result<Config, Error> {
                         debug!("{:?}", key);
                     }
                 }
+            } else if lower.as_str() == "host"
+                && value
+                    .split_whitespace()
+                    .any(|x| check_host_against_glob_pattern(host, x))
+            {
+                let mut c = Config::default(host);
+                c.port = 22;
+                config = Some(c)
             }
         }
     }
-    Ok(config)
+    if let Some(config) = config {
+        Ok(config)
+    } else {
+        Err(Error::HostNotFound)
+    }
 }
 
 fn check_host_against_glob_pattern(candidate: &str, glob_pattern: &str) -> bool {
